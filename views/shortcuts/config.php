3452deb13de9964327c13e92cceeb3cebadb57fc
Steve Joynt <steve@joynt.org.uk>
2021-08-23 12:50:24 +0000

    Added validation to prevent duplicate key assignments

--- views/shortcuts/config.php
+++ views/shortcuts/config.php
@@ -3,11 +3,17 @@ use humhub\compat\CActiveForm;
 use yii\helpers\Html;
 ?>
 <style>
-.shortcuts-table, td {
+#shortcuts-table td {
     padding: 4px;
 }
-#shortcuts-key, input {
+.shortcuts-key input {
     text-align: center;
+    text-transform: uppercase;
+}
+.shortcuts-error input {
+    text-align: center;
+    text-transform: uppercase;
+    background-color: #f88;
 }
 </style>
 <div class="panel panel-default">
@@ -27,21 +33,45 @@ CActiveForm::end();
     </div>
 </div>
 <script>
+
 function shortcutsFormToJson() {
     var table = document.getElementById("shortcuts-table");
     var menu = document.getElementById("space-main-menu");
     var items = menu.getElementsByTagName("A");
     var i;
     var shortcuts = {};
+    var usedKeys = "";
+    var badKeys = "";
     for ( i = 0 ; i < items.length ; i++ ) {
         var title = items[i].innerText.trim();
         var key = document.getElementById("shortcuts-key-" + i).value.trim().toUpperCase();
         shortcuts[title] = key;
+        if ( key ) {
+            if ( usedKeys.includes(key) ) {
+                badKeys += key;
+            } else {
+                usedKeys += key;
+            }
+        }
     }
-    document.getElementById("shortcuts-json").value = JSON.stringify(shortcuts);
-    return true;
+    if ( badKeys == "" ) {
+        document.getElementById("shortcuts-json").value = JSON.stringify(shortcuts);
+        return true;
+    } else {
+        for ( i = 0 ; i < items.length ; i++ ) {
+            var field = document.getElementById("shortcuts-key-" + i);
+            var key = field.value.trim().toUpperCase();
+            if ( key && badKeys.includes(key) ) {
+                field.parentElement.className = "shortcuts-error";
+            } else {
+                field.parentElement.className = "shortcuts-key";
+            }
+        }
+        return false;
+    }
 }
 document.getElementById("shortcuts-save").onclick = shortcutsFormToJson;
+
 function shortcutsJsonToForm() {
     var shortcuts = document.getElementById("shortcuts-json").value.trim();
     shortcuts = shortcuts ? shortcuts : '{}' ;
@@ -74,4 +104,5 @@ function shortcutsJsonToForm() {
     }
 }
 shortcutsJsonToForm();
+
 </script>
